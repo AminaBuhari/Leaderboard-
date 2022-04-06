d4aafcb41791bd1df9bea20b05181099f7d948c1
buhariaminat <buhariaminat@yahoo.com>
2022-04-06 11:10:26 +0100

    Update the linters error

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import form from './modules/form.js';
-import listHolder from './modules/formList.js'
+import listHolder from './modules/formList.js';
 import './style.css';
 
 function component() {
   const element = document.createElement('div');
-  element.classList.add('container')
-  
+  element.classList.add('container');
+
   element.appendChild(listHolder());
   element.appendChild(form());
 
